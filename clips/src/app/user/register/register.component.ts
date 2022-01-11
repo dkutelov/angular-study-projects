ff363516c9ab8e9d7b4d70fbc15b3d7279db1765
dkutelov <dkutelov@digitals.bg>
2022-01-11 19:46:03 +0200

    Reactive input component

--- clips/src/app/user/register/register.component.ts
+++ clips/src/app/user/register/register.component.ts
@@ -7,11 +7,19 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  // Declare outside FormGroup otherwise the props will have type AbstractControl
+  // new new FormControl -> type FormControl that we can pass down to Input component
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('');
+  age = new FormControl('');
+  password = new FormControl('');
+  phoneNumber = new FormControl('');
+
   registerForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    phoneNumber: new FormControl(''),
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    phoneNumber: this.phoneNumber,
   });
 }
