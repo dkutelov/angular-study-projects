26ad022473fc86371eb04b0aecc485bf334e8a89
dkutelov <dkutelov@digitals.bg>
2022-01-11 22:21:59 +0200

    Alert component

--- clips/src/app/user/register/register.component.ts
+++ clips/src/app/user/register/register.component.ts
@@ -26,6 +26,9 @@ export class RegisterComponent {
     Validators.minLength(13),
     Validators.maxLength(13),
   ]);
+  showAlert = false;
+  alertMsg = 'Please, wait! Your account is being created!';
+  alertColor = 'blue';
 
   registerForm = new FormGroup({
     name: this.name,
@@ -35,4 +38,11 @@ export class RegisterComponent {
     confirmPassword: this.confirmPassword,
     phoneNumber: this.phoneNumber,
   });
+
+  register() {
+    //reset
+    this.showAlert = true;
+    this.alertMsg = 'Please, wait! Your account is being created!';
+    this.alertColor = 'blue';
+  }
 }
--- clips/tailwind.config.js
+++ clips/tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
   prefix: "",
   content: ["./src/**/*.{html,ts}"],
+  safelist: ["bg-blue-400", "bg-green-400", "bg-red-400"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      opacity: ["disabled"],
+      backgroundColor: ["disabled"],
+    },
   },
   plugins: [],
 };
