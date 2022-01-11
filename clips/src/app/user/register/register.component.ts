cefd8a38ce28c68acf5f2aded83e9d57de7944b3
dkutelov <dkutelov@digitals.bg>
2022-01-11 21:42:11 +0200

    Reactive Forms - number and regex validation

--- clips/src/app/user/register/register.component.ts
+++ clips/src/app/user/register/register.component.ts
@@ -11,8 +11,16 @@ export class RegisterComponent {
   // new new FormControl -> type FormControl that we can pass down to Input component
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
   email = new FormControl('', [Validators.required, Validators.email]);
-  age = new FormControl('');
-  password = new FormControl('');
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120),
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/gm),
+  ]);
+  confirmPassword = new FormControl('', [Validators.required]);
   phoneNumber = new FormControl('');
 
   registerForm = new FormGroup({
@@ -20,6 +28,7 @@ export class RegisterComponent {
     email: this.email,
     age: this.age,
     password: this.password,
+    confirmPassword: this.confirmPassword,
     phoneNumber: this.phoneNumber,
   });
 }
