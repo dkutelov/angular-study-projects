e9b41d61e2e05ff3b885dec0a05dd243b5ead552
dkutelov <dkutelov@digitals.bg>
2022-01-18 21:40:56 +0200

    RxJS creation operators

---
+++ RxJS/src/basicObservableObserver.js
@@ -0,0 +1,80 @@
+//Observables: wrappers around async data source(mouse/ keyboard events, file uploads, http req) to filter, sort
+//Observers: receive the data from observables via a subscription(s)
+
+import { Observable } from "rxjs";
+
+//Observable: create new instance of Observable class
+const obsevable = new Observable((subscriber) => {
+  // push data to observer function next
+  subscriber.next("Hello, world");
+
+  //Throw error -> terminates the observable
+  subscriber.error("Error!");
+
+  // push next value
+  subscriber.next("Test");
+
+  // Complete the observable -> further next will not be executed
+  subscriber.complete();
+});
+
+//Observer
+obsevable.subscribe({
+  next: (value) => {
+    console.log(value);
+  },
+  complete: () => {
+    // no data can be passed
+    console.log("Completed!");
+  },
+  error: (err) => {
+    console.error(err);
+  }
+}); // pass observer with function next to handle the data
+
+//Async Observable
+const asyncObsevable = new Observable((subscriber) => {
+  const id = setInterval(() => {
+    subscriber.next("async test");
+    console.log("leak");
+  }, 1000);
+
+  //subscriber.complete(); // Stops the observable but setInterval continues
+
+  // clears setInterval;
+  return () => {
+    clearInterval(id);
+  };
+});
+
+console.log("--before--");
+
+//Observer - returns an object only to unsubscribe
+const subscription = asyncObsevable.subscribe({
+  next: (value) => {
+    console.log(value);
+  },
+  complete: () => {
+    // no data can be passed
+    console.log("Completed!");
+  },
+  error: (err) => {
+    console.error(err);
+  }
+});
+
+// Avoid memory leak by unsubscribing of the observer
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 4000);
+
+// Observable continues to emit. We can subscribe again:
+// setTimeout(() => {
+//   asyncObsevable.subscribe({
+//     next: (value) => {
+//       console.log(value);
+//     }
+//   });
+// }, 10000);
+
+console.log("--after--");
---
+++ RxJS/src/creationOperators.js
@@ -0,0 +1,35 @@
+import { interval, timer, fromEvent, of, from } from "rxjs";
+
+// Declarative Writing Of Observable
+// RxJS Operator is a function
+
+// Creation Operators - creates observable from any source of data
+// 1. Interval
+//const observable = interval(1000); // setIntrval wrapped with an onservable
+
+// 2. Timer - emits first value
+// const observable = timer(1000, 1000); // first - 1000 - after one sec, second 1000 is interval
+
+// 3. DOM event
+//const observable = fromEvent(document, "click"); // first is selector, second is type of event
+// should manually unsubscribe
+
+// 4. of - loops through the values
+//const observable = of(1, 2, 3, 4, 5);
+//const observable = of([1, 2, 3, 4, 5]); // no looping
+// Values are not emitted before a subscription; after values are pushed the observable is completed
+
+// 5. from - flattens the array, split string
+//const observable = from([1, 2, 3, 4, 5]); // loops
+const observable = from(fetch("https://jsonplaceholder.typicode.com/todos/1")); // waits the promise to resolve
+
+// Subscription
+// const subscription = observable.subscribe(console.log); // one function is treated as next function
+const subscription = observable.subscribe({
+  next(value) {
+    console.log(value);
+  },
+  complete() {
+    console.log("completed");
+  }
+}); // one function is treated as next function
