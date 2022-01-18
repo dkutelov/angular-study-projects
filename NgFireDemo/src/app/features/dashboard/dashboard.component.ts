0db56d0587c0ad86a033d7516272623787fb111c
dkutelov <dkutelov@digitals.bg>
2022-01-18 11:08:45 +0200

    Logout user

--- NgFireDemo/src/app/features/dashboard/dashboard.component.ts
+++ NgFireDemo/src/app/features/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logout() {
+    return this.authService
+      .logout()
+      .then(() => this.router.navigate(['/']))
+      .catch((e) => console.log(e.message));
   }
-
 }
