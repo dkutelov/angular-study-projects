af95686b61f8dd75c0f617936a5803ac16ede07d
dkutelov <dkutelov@digitals.bg>
2022-01-19 10:12:22 +0200

    RxJS Flattening operators

--- RxJS/src/index.js
+++ RxJS/src/index.js
@@ -1,59 +1,82 @@
-import { of, fromEvent, interval } from "rxjs";
-import { map, pluck, filter, reduce, take, scan, tap } from "rxjs/operators";
+import { fromEvent, interval } from "rxjs";
+import { map, mergeMap, switchMap, take, tap } from "rxjs/operators";
+import { ajax } from "rxjs/ajax";
 
-// Pipable operators - accept observable and output another observable
-// Transforming, filtering and combiming data
-// Pipe function passes the observable to each operator in the set order
-
-// 1. Map - like array map but on observable
-//const observable = of(1, 2, 3, 4, 5).pipe(map((value) => `$${value}`)); //$1,$2,$3,$4,$5
-
-// Subscribe to the observable outputed from pipe and map
-// The original observable is unaffected
-//const observable = of(1, 2, 3, 4, 5);
-//const numbersWithSymbol = observable.pipe(map((value) => `$${value}`));
-//const subscription = observable.subscribe(console.log); //1,2,3,4,5
-//numbersWithSymbol.subscribe(console.log); //$1,$2,$3,$4,$5
-
-// 2. Pluck - get a value from object
-// const observable = fromEvent(document, "keydown").pipe(
-//map((event) => event.code)
-//pluck("code")
-//);
-//observable.subscribe(console.log); // KeyF, etc
-
-// 3. Filter operator - value should meet the requirement
-// const observable = fromEvent(document, "keydown").pipe(
-//   pluck("code"),
-//   filter((code) => code === "Space") //return boolean value; if true it will pass the value from prev pipe operator
-// );
-// observable.subscribe(console.log); // KeyF, etc
-
-// 4. Reduce operator
-// accumolates all values and when the observable is completed it pushes the output
-// const observable = of(1, 2, 3, 4, 5).pipe(
-//   reduce((acc, value) => acc + value, 0)
-// );
-// observable.subscribe(console.log);
-
-// 5. Take operator - takes values up to a limit and then it ignores further value
-// const observable = interval(500).pipe(
-//   take(5), // order matters; should check threshould before reduce; 0 is first value
-//   reduce((acc, value) => acc + value, 0)
-// );
-// observable.subscribe(console.log);
-
-// 6. Scan - same as reduced but accum value is pushed to next observable
-// const observable = interval(500).pipe(
-//   take(5), // order matters; should check threshould before reduce; 0 is first value
-//   scan((acc, value) => acc + value, 0)
-// );
-// observable.subscribe(console.log);
-
-//7. Tap operator - for debugging; does not impact the value
-const observable = interval(500).pipe(
-  take(5),
-  tap(console.log), //spy on values
-  reduce((acc, value) => acc + value, 0)
+// Flattening - create observable within observable
+const button = document.querySelector("#btn");
+const observableBtnClick = fromEvent(button, "click").pipe(
+  map(() => {
+    //ajax is creation operator and can not be called directly in pipe
+    return ajax.getJSON("https://jsonplaceholder.typicode.com/todos/1");
+  })
 );
-observable.subscribe(console.log);
+
+// Subscribe to click event -> returns observable
+observableBtnClick.subscribe({
+  next(value) {
+    //value.subscribe(console.log); // subscribe to ajax get -> returns the result of get
+  }
+}); // returns observable
+
+// Flattening operators - do not need to subscribe to inner observable
+
+// 1. Merge Map
+// Always should control inner observable to complete
+const observableBtnClick2 = fromEvent(button, "click").pipe(
+  mergeMap(() => {
+    // mergeMap subscribes to the observable returned by the function
+    return ajax.getJSON("https://jsonplaceholder.typicode.com/todos/1");
+    //return interval(1000).pipe(tap(console.log), take(5)); // takes completes here only inner observable
+  })
+  //take(5) // if no limit interval will continue to run; each btn click will create new interval
+  // here take completes the fromEvent observable and then clicking the button does not do anything
+);
+
+// observableBtnClick2.subscribe({
+//   next(value) {
+//     console.log(value);
+//   },
+//   complete() {
+//     console.log("Completed");
+//   }
+// });
+
+// 2. Switch Map
+// active inner observables is limited to one - manage one active subscription at a time; each time we click the button, any previous interval will be completed
+// safest flattening operator
+// use for http requests to cancel any previous active requests
+const observableBtnClick3 = fromEvent(button, "click").pipe(
+  switchMap(() => {
+    return interval(1000).pipe(
+      take(5),
+      tap({
+        complete() {
+          console.log("Inner observable completed");
+        }
+      })
+    );
+  })
+);
+
+observableBtnClick3.subscribe({
+  next(value) {
+    console.log(value);
+  },
+  complete() {
+    console.log("Completed");
+  }
+});
+
+// 3. Concat Map
+// works like switchMap but does not cancel previous innerobservables -> it puts them in a queue; second observable will start after first is completed
+// queuing observables with limited time span
+
+// 4. Exhaust Map
+// ignors inner observable if there is currently not completed inner observable
+// suitable for form submission to block any additional submissions until first is completed
+
+// Summary - Restaurant example
+// switchMap - stop working on current order and start new order. Only the latest will be completed.
+// concatMap - orders are added to the queue. Once you finish a order, you move to the next one. All completed, one after the other.
+// mergeMap - work on all orders at the same time as soon as they are give you.
+// exhaustMap - ignore new orders before you finish the current one. Once you finish, you are open to accept new orders.
