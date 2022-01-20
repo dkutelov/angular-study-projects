342a8d73f44cea8fd5848048f3b925e11c44b30c
dkutelov <dkutelov@digitals.bg>
2022-01-20 21:39:38 +0200

    Write user uid to firebase

--- clips/src/app/services/auth.service.ts
+++ clips/src/app/services/auth.service.ts
@@ -1,29 +1,50 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private userCollection: AngularFirestoreCollection<IUser>;
+
   constructor(
     private angularFireAuth: AngularFireAuth,
     private db: AngularFirestore
-  ) {}
+  ) {
+    this.userCollection = db.collection('users');
+  }
 
   public async createUser(userData: IUser) {
+    if (!userData.password) {
+      throw new Error('Password not provided!');
+    }
+
     const userCred = await this.angularFireAuth.createUserWithEmailAndPassword(
       userData.email,
       userData.password
     );
 
-    //creates collection if does not exists
-    await this.db.collection('users').add({
+    if (!userCred.user) {
+      throw new Error('User can not be found!');
+    }
+
+    const uid = userCred.user.uid;
+
+    await this.userCollection.doc(uid).set({
       name: userData.name,
       email: userData.email,
       age: userData.age,
       phoneNumber: userData.phoneNumber,
+    });
+
+    //update display name or profilePhoto in auth profile
+    await userCred.user.updateProfile({
+      displayName: userData.name,
     });
   }
 }
