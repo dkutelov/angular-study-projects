fb714a5eb050cc9b82946d43f1669f66fbd528c7
dkutelov <dkutelov@digitals.bg>
2022-01-21 18:32:33 +0200

    Login user

--- clips/src/app/user/login/login.component.ts
+++ clips/src/app/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,35 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  login(form: NgForm) {
+  showAlert = false;
+  alertMsg = 'Please, wait! You are logged in!';
+  alertColor = 'blue';
+  inSubmission = false;
+
+  constructor(private angularFireAuth: AngularFireAuth) {}
+
+  async login(form: NgForm) {
     if (form.valid) {
-      console.log(form);
-      console.log(form.value.email);
+      this.showAlert = true;
+      this.alertMsg = 'Please, wait! You are logged in!'; // always reset values
+      this.alertColor = 'blue';
+      this.inSubmission = true;
+
+      const { email, password } = form.value;
+
+      try {
+        await this.angularFireAuth.signInWithEmailAndPassword(email, password);
+      } catch (error) {
+        console.error(error);
+        this.alertMsg =
+          'An unexprected error occured! Please, try again later.';
+        this.alertColor = 'red';
+        this.inSubmission = false;
+        return;
+      }
+
+      this.alertMsg = 'Success! Your are logged in.';
+      this.alertColor = 'green';
     }
   }
 }
