1dad4dfdaf2cf0566bb7dac05c97c24c92c242e8
dkutelov <dkutelov@digitals.bg>
2022-01-26 21:34:03 +0200

    Basic routing home and about page

--- clips/src/app/app.module.ts
+++ clips/src/app/app.module.ts
@@ -10,9 +10,11 @@ import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
 import { environment } from 'src/environments/environment';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, AboutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
